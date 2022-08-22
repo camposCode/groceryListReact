737073416662f431869f38d268e0b06e8b2b0dcf
camposCode <info@josecampos.io>
2022-08-22 18:58:50 +0200

    checkboxes in content created

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
+    const[items, setItems] = useState([
+        {
+            id: new Date().getTime(),
+            checked: false,
+            item: 'milch'
+        },
+        {
+            id: new Date().getFullYear(),
+            checked: false,
+            item: 'eggs'
+        },
+        {
+            id: new Date().getDay(),
+            checked: false,
+            item: 'sugar'
+        }
+    ]);
+    
     return (
         <main>
-            <p>hello Kenaz</p>
+            <ul >
+                {
+                    items.map(item =>(
+                        <li 
+                            key={ item.id }
+                            className='item'
+                        >
+                                <input 
+                                    type="checkbox" 
+                                    checked={ item.checked }
+                                />
+                                <label htmlFor="">{ item.item }</label>
+                                <button>Delete</button>
+                        </li>
+                    ))
+                }
+                
+            </ul>
         </main>
     )
 }
 
 export default Content
+            
+
