8c3daf60a84e530e443f886e2cbf4b562714865c
camposCode <info@josecampos.io>
2022-08-27 12:47:24 +0200

    PATCH method created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import {useEffect, useState} from "react";
+import apiRequest from "./api.request";
 import AddItem from "./components/AddItem";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SearchItem from "./components/SearchItem";
-
 
 function App() {
     const API_URL = 'http://localhost:3500/items';
@@ -13,42 +13,65 @@ function App() {
 
     const [newItem, setNewItem] = useState('');
     const [search, setSearch] = useState('');
+    const [fetchError, setFetchError] = useState(null);
+    const[isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const fetchItems = async() =>{
-            try{
+
+        const fetchItems = async () => {
+            try {
                 const response = await fetch(API_URL);
+                if (!response.ok) throw Error('Did not receive expected data');
                 const listItems = await response.json();
-                console.log(listItems);
                 setItems(listItems);
-            }catch(err){
-                console.log(err.stack)
+                setFetchError(null);
+            } catch (err) {
+                setFetchError(err.message);
+            } finally {
+                setIsLoading(false);
             }
+            }
+        
+            setTimeout(() => fetchItems(), 500);
+        
+        }, [])
+        
+        const addItem = async (item) => {
+            const id = items.length ? items[items.length - 1].id + 1 : 1;
+            const myNewItem = { id, checked: false, item };
+            const listItems = [...items, myNewItem];
+            setItems(listItems);
+        
+            const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(myNewItem)
+            }
+            const result = await apiRequest(API_URL, postOptions);
+            if (result) setFetchError(result);
         }
+    
 
-        (async() => await fetchItems())();
-    }, []);
-
-    const addItem = (item) => {
-        const id = items.length ? items[items.length - 1].id + 1 : 1;
-        const newItem = {
-            id,
-            checked: false,
-            item
-        };
-        const listItems = [
-            ...items,
-            newItem
-        ];
-        setItems(listItems);
-    }
-
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
         const listItems = items.map((item) => item.id === id ? {
             ...item,
             checked: !item.checked
         } : item);
         setItems(listItems);
+
+        const myItem = listItems.filter((item) => item.id === id);
+        const updateOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ checked: myItem[0].checked })
+        };
+        const reqUrl= `${ API_URL }/${ id }`;
+        const result = await apiRequest(reqUrl, updateOptions);
+        if (result) setFetchError(result);
     }
 
     const handleDelete = (id) => {
@@ -73,11 +96,17 @@ function App() {
                 handleSubmit={handleSubmit}/>
             <SearchItem search={search}
                 setSearch={setSearch}/>
-            <Content items={
-                    items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))
+            <main>
+                { isLoading &&  <p>Loading Items...</p> }
+                {
+                    fetchError && <p style={{ color: "red" }}>{ `Error: ${fetchError}` }</p>
                 }
-                handleCheck={handleCheck}
-                handleDelete={handleDelete}/>
+                { !fetchError && !isLoading && <Content items={
+                        items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))
+                    }
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}/>}
+            </main>
             <Footer/>
         </div>
     );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,20 +4,21 @@ const Content = ({items, handleCheck, handleDelete}) => {
 
 
     return (
-        <main> {
-            items.length ? (
-                <ItemList 
-                    items={ items }
-                    handleCheck={ handleCheck }
-                    handleDelete={ handleDelete }
-                />
-            ) : (
-                <p style={
-                    {marginTop: '2rem'}
-                }>Your list is empty</p>
-            )
-        } 
-        </main>
+        <> 
+            {
+                items.length ? (
+                    <ItemList 
+                        items={ items }
+                        handleCheck={ handleCheck }
+                        handleDelete={ handleDelete }
+                    />
+                ) : (
+                    <p style={
+                        {marginTop: '2rem'}
+                    }>Your list is empty</p>
+                )
+            } 
+        </>
     )
 }
 
